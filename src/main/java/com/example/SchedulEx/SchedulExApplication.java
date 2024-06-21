c4bff5faf39d38ad74ab51cd1f00313ee0372f15
onioin <canyongudaitis@gmail.com>
2024-06-21 14:20:40 -0700

    initial commit

---
+++ src/main/java/com/example/SchedulEx/SchedulExApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchedulEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SchedulEx/SchedulExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SchedulEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
