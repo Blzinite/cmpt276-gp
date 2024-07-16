1241121006919ccbbaf50fab47fae5da1b20c04d
onioin <canyongudaitis@gmail.com>
2024-07-15 22:30:56 -0700

    forgot to git add

---
+++ src/main/java/com/example/SchedulEx/models/RequestStatus.java
@@ -0,0 +1,9 @@
+package com.example.SchedulEx.models;
+
+public class RequestStatus {
+    public static final int PENDING = 700;
+    public static final int ACCEPTED_TIME_ONE = 701;
+    public static final int ACCEPTED_TIME_TWO = 702;
+    public static final int ACCEPTED_TIME_THREE = 703;
+    public static final int REJECTED = 704;
+}
