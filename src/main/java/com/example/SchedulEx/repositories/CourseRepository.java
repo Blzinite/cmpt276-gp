6d8740426c004f5c44745b55e702c1de443785a3
Canyon Gudaitis <102696701+onioin@users.noreply.github.com>
2024-07-16 11:03:26 -0700

    Update CourseRepository.java
    bugfix, see commit 437b88d

--- src/main/java/com/example/SchedulEx/repositories/CourseRepository.java
+++ src/main/java/com/example/SchedulEx/repositories/CourseRepository.java
@@ -1,6 +1,7 @@
 package com.example.SchedulEx.repositories;
 
 import com.example.SchedulEx.models.Course;
+import com.example.SchedulEx.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface CourseRepository extends JpaRepository<Course, Integer>
 {
     Optional<Course> findByCourseName(String courseName);
     List<Course> findByCourseNameNot(String courseName);
+    List<Course> findByInstructor(User instructor);
 }
