3c880760d0415077d690bc2e9bf2b7db170de1de
Blzinite <rf.blzinite@gmail.com>
2024-08-01 21:02:13 -0700

    small bug fix - clicking on dates with other classes caused an error

--- src/main/resources/static/js/calendarMonth.js
+++ src/main/resources/static/js/calendarMonth.js
@@ -11,7 +11,7 @@ const calendarEntries = document.getElementsByClassName("calendarEntry");
 for (let entry of calendarEntries) {
     entry.addEventListener('click', function()
     {
-        let day = entry.textContent.replace(/\D/g, "");;
+        let day = entry.getElementsByClassName("date-entry")[0].textContent.replace(/\D/g, "");;
         console.log(day);
         let date = year+"-"+String(monthDict.indexOf(month)+1).padStart(2, "0")+"-"+String(day).padStart(2, "0");
         index.monthCalendarCall(date);
